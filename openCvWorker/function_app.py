1e667ada7023a72ddecdd7f897b14f1c9b103d95
Fanis <126913238+Fanis987@users.noreply.github.com>
2025-05-18 18:42:58 +0300

    Updated requirements and added structure for the open cv analysis

--- openCvWorker/function_app.py
+++ openCvWorker/function_app.py
@@ -1,5 +1,8 @@
 import azure.functions as func
 import logging
+import os
+import tempfile
+from openCvHelper import analyze_video
 
 app = func.FunctionApp()
 
@@ -23,7 +26,20 @@ def blob_trigger_opencv_processing_function(segmentBlob: func.InputStream):
     file_name = segmentBlob.name
     size = segmentBlob.length
     logging.info(f"Blob trigger fired for: {file_name}, size: {size} bytes")
-    print("test")
 
-    # Using Content with open CV
-    #content : bytes = segmentBlob.read() #bytes of the file uploaded
+    # Saving a copy of the video segment locally
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
+        tmp.write(segmentBlob.read())
+        save_path = tmp.name  # Full safe path like /tmp/tmpx83k9sd3.mp4
+    
+    if not os.path.exists(save_path):
+        logging.error(f"Error video not saved successfully!")
+        return
+    logging.info(f"File saved successfully {save_path}") 
+          
+    # Analyze video contents with open CV
+    output = analyze_video() # UNDER CONSTRUCTION
+
+    # Delete temporary video file
+    os.remove(save_path)
+    logging.info("Temp file deleted")
---
+++ openCvWorker/openCvHelper.py
@@ -0,0 +1,10 @@
+import time
+import logging
+
+# This function will use open cv to analyze the 2-min video segement
+def analyze_video():
+    logging.info(" analysis started")
+    time.sleep(0.5)
+    logging.info(" analysis ended ")
+    
+    

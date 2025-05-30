5756a7a390324b35005f5fdb4222c7020a7b303f
Fanis <126913238+Fanis987@users.noreply.github.com>
2025-05-30 15:19:38 +0300

    added csv parsing functionality

--- intermediateWorker/function_app.py
+++ intermediateWorker/function_app.py
@@ -2,6 +2,8 @@ import azure.functions as func
 import datetime
 import json
 import logging
+import csv
+import io
 
 app = func.FunctionApp()
 
@@ -11,7 +13,35 @@ app = func.FunctionApp()
                   path="intermediate-results/{name}.csv", #non-csv filtered out
                   connection="AzureWebJobsStorage") 
 def BlobTriggerFunc(uploadedFile: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-                f"Name: {uploadedFile.name}"
-                f"Blob Size: {uploadedFile.length} bytes")
+    
+    # Parsing the uploaded CSV file
+    try:
+        blob_text = uploadedFile.read().decode('utf-8')
+        vehicle_records_list = []
+
+        # read each line into nonymous object and store it
+        reader = csv.DictReader(io.StringIO(blob_text))
+        for row in reader:
+            record = {
+            'timeEntered': float(row['timeEntered']),
+            'speed': float(row['speed']),
+            'vehicleType': row['vehicleType'],
+            'lane': row['lane'],
+            'speeding': int(row['speeding'])}
+            vehicle_records_list.append(record)
+
+        vehicle_records_num : int = len(vehicle_records_list)
+        if(vehicle_records_num == 0):
+            logging.error(f"The uploaded file had no info on vehicles ")
+            return
+        logging.info(f"Found {vehicle_records_num} vehicle records")
+    
+    except KeyError:
+        logging.error(f"The uploaded csv file had incorrect headers ")
+        return
+    
+    except Exception as e:
+        logging.error(f"Exception occured: {e} ")
+        return
+    
     

ec3ed66f2bccaa67fa1ac3ae485ed8adc4a48ad5
Konstantinos Falaras <61200126+KostasFalaras@users.noreply.github.com>
2025-06-16 20:08:49 +0300

    Delete openCvWorker directory

--- openCvWorker/cv_tester.py
+++
@@ -1,4 +0,0 @@
-from process import analyze_video
-
-video_path = "clip_0.mp4"
-analyze_video(video_path)
--- openCvWorker/cv_tester2.py
+++
@@ -1,4 +0,0 @@
-from process2 import analyse_clip
-
-# With video display
-analyse_clip("../videos_clips_csvs/10s_clips/clip_1.mp4", "output/results.csv", show_video=True)
